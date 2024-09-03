2dca116752596075fadccdc52ca21d798a991b13
Jason Brezenski <jasonbrezenski@@yahoo.com>
2024-09-03 11:50:31 -0500

    College Football Mascots

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace LINQ
+﻿using System.Linq;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("College Football Mascots:\n");
+
+            var collegeTeams = new List<string>() { "Huskers", "Hawkeyes", "Sooners", "Longhorns", "Crimson Tide", "Buckeyes", "Ducks", "Bruins" };
+
+            var orderCollegeTeams = collegeTeams.OrderByDescending(x => x.Length).ToList();
+            orderCollegeTeams.ForEach(x => Console.WriteLine(x));
         }
     }
+
 }
